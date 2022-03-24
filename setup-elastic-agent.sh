3b53fc8ecba9cb9221f312fd0a26357be37d153f
Muhammad Abdullah Abid <abdullah@autonomoustech.ca>
2022-03-23 22:11:29 -0700

    Create setup-elastic-agent.sh

---
+++ setup-elastic-agent.sh
@@ -0,0 +1,7 @@
+sudo mkdir /usr/src/shared
+sudo chmod 777 /usr/src/shared
+cd /usr/src/shared
+wget https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-7.16.3-linux-x86_64.tar.gz
+tar zxvf elastic-agent-7.16.3-linux-x86_64.tar.gz
+cd elastic-agent-7.16.3-linux-x86_64
+sudo ./elastic-agent install --insecure --url=http://pine.tokenocean.io:8220 --enrollment-token=Y25iV2ozOEJVVkdWNE5oQWdYS286QTRrSzU2cThTSHlBWlJVRlY2YVNUUQ==
