3e8b1c891cfda6bc2510051a31c70075601609cc
Muhammad Abdullah Abid <abdullah@autonomoustech.ca>
2022-03-23 22:16:41 -0700

    Update setup-elastic-agent.sh

--- setup-elastic-agent.sh
+++ setup-elastic-agent.sh
@@ -4,4 +4,4 @@ cd /usr/src/shared
 wget https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-7.16.3-linux-x86_64.tar.gz
 tar zxvf elastic-agent-7.16.3-linux-x86_64.tar.gz
 cd elastic-agent-7.16.3-linux-x86_64
-sudo ./elastic-agent install --insecure --url=http://pine.tokenocean.io:8220 --enrollment-token=Y25iV2ozOEJVVkdWNE5oQWdYS286QTRrSzU2cThTSHlBWlJVRlY2YVNUUQ==
+sudo ./elastic-agent install --insecure --force --v --url=http://pine.tokenocean.io:8220 --enrollment-token=Y25iV2ozOEJVVkdWNE5oQWdYS286QTRrSzU2cThTSHlBWlJVRlY2YVNUUQ==
