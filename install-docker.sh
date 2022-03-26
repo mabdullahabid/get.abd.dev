c21dd481bef95b9e95a41d8bac21636473b21bb9
Muhammad Abdullah Abid <abdullah@autonomoustech.ca>
2022-03-25 21:03:52 -0700

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,1 +1,8 @@
-# install-docker.sh
+#!/bin/bash
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker $USER
+sudo systemctl enable docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
